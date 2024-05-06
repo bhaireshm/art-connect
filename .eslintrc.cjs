535f5ac34662d3df397fb3c69dd74d36ecf57091
Bhairesh M <bhairesh@arithaconsulting.com>
2024-05-06 20:28:58 +0530

    :tada: Initial Commit.

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  extends: ['mantine', 'plugin:@next/next/recommended', 'plugin:jest/recommended'],
+  plugins: ['testing-library', 'jest'],
+  overrides: [
+    {
+      files: ['**/?(*.)+(spec|test).[jt]s?(x)'],
+      extends: ['plugin:testing-library/react'],
+    },
+  ],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  rules: {
+    "semi": "warn",
+    "quotes": ["warn", "double"],
+    'react/react-in-jsx-scope': 'off',
+    'import/extensions': 'off',
+    'linebreak-style': ["error", "windows"],
+    "@typescript-eslint/quotes": [
+      "error",
+      "double"
+    ]
+  },
+};
---
+++ .prettierrc.cjs
@@ -0,0 +1,9 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  "singleQuote": false,
+  "jsxSingleQuote": false,
+  "endOfLine": "crlf",
+  "printWidth": 100,
+  "trailingComma": "es5",
+  // "extends": require('eslint-config-mantine/.prettierrc.js'),
+};

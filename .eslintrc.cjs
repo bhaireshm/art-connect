72c6cdc1539e66dab3662808b9fe701649759bc1
Bhairesh M <bhairesh@arithaconsulting.com>
2024-05-14 14:23:35 +0530

    initial commit

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,24 +1,22 @@
 module.exports = {
-  extends: ['mantine', 'plugin:@next/next/recommended', 'plugin:jest/recommended'],
-  plugins: ['testing-library', 'jest'],
+  extends: ["mantine", "plugin:@next/next/recommended", "plugin:jest/recommended"],
+  plugins: ["testing-library", "jest"],
   overrides: [
     {
-      files: ['**/?(*.)+(spec|test).[jt]s?(x)'],
-      extends: ['plugin:testing-library/react'],
+      files: ["**/?(*.)+(spec|test).[jt]s?(x)"],
+      extends: ["plugin:testing-library/react"],
     },
   ],
   parserOptions: {
-    project: './tsconfig.json',
+    project: "./tsconfig.json",
   },
   rules: {
-    "semi": "warn",
-    "quotes": ["warn", "double"],
-    'react/react-in-jsx-scope': 'off',
-    'import/extensions': 'off',
-    'linebreak-style': ["error", "windows"],
-    "@typescript-eslint/quotes": [
-      "error",
-      "double"
-    ]
+    semi: "warn",
+    quotes: ["warn", "double"],
+    "react/react-in-jsx-scope": "off",
+    "import/extensions": "off",
+    "linebreak-style": ["warn", "windows"],
+    "@typescript-eslint/quotes": ["error", "double"],
   },
 };
+

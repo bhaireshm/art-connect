c7a525dfd93e3876435bc8703d1d6eba1d8885bf
Bhairesh M <bhairesh@arithaconsulting.com>
2024-05-21 22:46:31 +0530

    update: redux and eslint.

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,4 +1,4 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, type Reducer } from "@reduxjs/toolkit";
 
 export const store = () => configureStore({
   reducer: {},
@@ -9,3 +9,18 @@ export type AppStore = ReturnType<typeof store>;
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<AppStore["getState"]>;
 export type AppDispatch = AppStore["dispatch"];
+
+type ReducerType = {
+  [name: string]: Reducer;
+};
+
+/**
+ * TODO
+ * Refer the below link and implement the whole reducer injection
+ * @link https://redux.js.org/usage/code-splitting#defining-an-injectreducer-function
+ */
+export function addReducer(reducer: ReducerType) { // ReducerType[]
+  Object.keys(reducer).forEach(reducerName => {
+    store().replaceReducer(reducer[reducerName]);
+  });
+}

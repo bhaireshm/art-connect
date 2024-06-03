616b450b95d2b56c636913c15dd0440e9821c393
Bhairesh M <bhairesh@arithaconsulting.com>
2024-06-03 11:45:28 +0530

    configs: :wrench: git-hooks and settings.json updated.

--- .hooks/prepare.sh
+++ .hooks/prepare.sh
@@ -1,6 +1,6 @@
 #!/usr/bin sh
 
-echo  "Preparing git config..."
+# echo  "Preparing git config..."
 
 git config --local user.name "Bhairesh M"
 git config --local user.email "bhairesh@arithaconsulting.com"

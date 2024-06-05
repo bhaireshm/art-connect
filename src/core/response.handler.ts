c7273d158150a95581ee9614589407a849743346
Bhairesh M <bhairesh@arithaconsulting.com>
2024-06-05 10:42:29 +0530

    (api) update: response handler class modified.

--- src/core/response.handler.ts
+++ src/core/response.handler.ts
@@ -1,6 +1,31 @@
+import { NextResponse } from "next/server";
+
 class ResponseHandler {
-  static status(status: number, statusText = "") {
-    return { status, statusText };
+  data: any;
+  error?: any;
+  status?: number;
+  statusText?: string;
+
+  constructor(data: any, error?: any, status?: number, statusText?: string) {
+    this.data = data;
+    this.error = error;
+    this.status = status;
+    this.statusText = statusText;
+  }
+
+  static success(data: any, status?: number, statusText?: string) {
+    return new ResponseHandler(data, null, status, statusText).json();
+  }
+
+  static error(error: any, status?: number, statusText?: string) {
+    return new ResponseHandler(null, error, status, statusText).json();
+  }
+
+  json(): NextResponse {
+    return NextResponse.json(
+      { data: this.data, error: this.error, status: this.status },
+      { status: this.status, statusText: this.statusText },
+    );
   }
 }
 

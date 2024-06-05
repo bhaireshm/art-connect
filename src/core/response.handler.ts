5b276a629d74822f3fc0acfddfcee8cc3b080a99
Bhairesh M <bhairesh@arithaconsulting.com>
2024-06-05 13:03:50 +0530

    (api) update: response.handler error and success methods made generic.

--- src/core/response.handler.ts
+++ src/core/response.handler.ts
@@ -1,29 +1,56 @@
+import { STATUS_CODES } from "@/utils/constants";
 import { NextResponse } from "next/server";
 
 class ResponseHandler {
   data: any;
   error?: any;
   status?: number;
+  message?: string;
   statusText?: string;
 
-  constructor(data: any, error?: any, status?: number, statusText?: string) {
+  constructor(data: any, error?: any, message?: string, status?: number) {
     this.data = data;
     this.error = error;
-    this.status = status;
-    this.statusText = statusText;
+    this.message = message;
+
+    if (status) {
+      this.status = status;
+      this.statusText = STATUS_CODES[status];
+    }
   }
 
-  static success(data: any, status?: number, statusText?: string) {
-    return new ResponseHandler(data, null, status, statusText).json();
+  static success(data: any, arg2?: string | number, arg3?: number): NextResponse {
+    let message = "";
+    let status = 200;
+
+    if (typeof arg2 === "string") {
+      message = arg2;
+      if (typeof arg3 === "number") status = arg3;
+    } else if (typeof arg2 === "number") status = arg2;
+
+    return new ResponseHandler(data, null, message, status).json();
   }
 
-  static error(error: any, status?: number, statusText?: string) {
-    return new ResponseHandler(null, error, status, statusText).json();
+  static error(error: any, arg2?: string | number, arg3?: number): NextResponse {
+    let message = "";
+    let status = 500;
+
+    if (typeof arg2 === "string") {
+      message = arg2;
+      if (typeof arg3 === "number") status = arg3;
+    } else if (typeof arg2 === "number") status = arg2;
+
+    return new ResponseHandler(null, error, message, status).json();
   }
 
   json(): NextResponse {
     return NextResponse.json(
-      { data: this.data, error: this.error, status: this.status },
+      {
+        data: this.data,
+        message: this.message,
+        status: this.status,
+        error: this.error,
+      },
       { status: this.status, statusText: this.statusText },
     );
   }

1915cfa454c180cbf2309291dc95f9e412c3e68b
Bhairesh M <bhairesh@arithaconsulting.com>
2024-06-11 15:05:05 +0530

    configs: updated.

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    esmExternals: "loose", // <-- add this
-    serverComponentsExternalPackages: ["mongoose"] // <-- and this
+    esmExternals: "loose",
+    serverComponentsExternalPackages: ["mongoose"],
+    optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
   },
 
   // and the following to enable top-level await support for Webpack

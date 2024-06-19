f66ba66af845d027eb5b1686175eabcb1276927a
Bhairesh M <bhairesh@arithaconsulting.com>
2024-06-19 23:28:22 +0530

    config: Image config added

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,18 @@ const nextConfig = {
     optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
   },
 
+   // Image Config
+   images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/photo-**",
+      },
+    ],
+  },
+
   // and the following to enable top-level await support for Webpack
   // webpack: (config) => {
   //   config.experiments = {

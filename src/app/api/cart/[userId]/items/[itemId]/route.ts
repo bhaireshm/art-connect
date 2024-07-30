0f0ef1290d14199c1c37b14dc105fc97e42d7e09
Bhairesh M <bhairesh@arithaconsulting.com>
2024-07-31 00:47:48 +0530

    (api) update: DELETE /api/cart/[userId]/items/[itemId] implemented.

--- src/app/api/cart/[userId]/items/[itemId]/route.ts
+++ src/app/api/cart/[userId]/items/[itemId]/route.ts
@@ -1,38 +1,27 @@
-// "use server";
+"use server";
 
-// import { ResponseHandler } from "@/core";
-// import DatabaseError from "@/database/db.error";
-// import { Cart } from "@/modules";
-// import { NextRequest } from "next/server";
-
-/** /api/cart/[userId]/items/[itemId]/route.ts */
-// export async function PUT(req: NextRequest, { params }: { params: { userId: string, itemId: string } }) {
-//   try {
-//     const data = await req.json();
-//     const cart = await Cart.findOneAndUpdate(
-//       { user: new ObjectId(params.userId), "items._id": new ObjectId(params.itemId) },
-//       { $set: { "items.$.quantity": data.quantity } },
-//       { new: true }
-//     );
-//     return ResponseHandler.success(cart);
-//   } catch (error) {
-//     const err = new DatabaseError(error);
-//     return ResponseHandler.error(err, err.code);
-//   }
-// }
+import { ResponseHandler } from "@/core";
+import DatabaseError from "@/database/db.error";
+import { Cart } from "@/modules";
+import { NextRequest } from "next/server";
 
 
-/** /api/cart/[userId]/items/[itemId] */
-// export async function DELETE(req: NextRequest, { params }: { params: { userId: string, itemId: string } }) {
-//   try {
-//     const cart = await Cart.findOneAndUpdate(
-//       { user: new ObjectId(params.userId) },
-//       { $pull: { items: { _id: new ObjectId(params.itemId) } } },
-//       { new: true }
-//     );
-//     return ResponseHandler.success(cart);
-//   } catch (error) {
-//     const err = new DatabaseError(error);
-//     return ResponseHandler.error(err, err.code);
-//   }
-// }
+/**
+ * Remove Item from Cart
+ * DELETE /api/cart/[userId]/items/[itemId]
+ * 
+ * Removes a specific item from the user's cart.
+ */
+export async function DELETE(req: NextRequest, { params }: { params: { userId: string, itemId: string } }) {
+  try {
+    const cart = await Cart.update(
+      { user: params.userId },
+      { $pull: { items: { _id: params.itemId } } },
+      { new: true }
+    );
+    return ResponseHandler.success(cart);
+  } catch (error) {
+    const err = new DatabaseError(error);
+    return ResponseHandler.error(err, err.code);
+  }
+}

f35c2fd781be9f55e319e648d4069408878a214e
Bhairesh M <bhairesh@arithaconsulting.com>
2024-08-11 22:29:37 +0530

    (api) fix: GET /api/cart/[userId] - artwork.artist populate issue.

--- src/app/api/cart/[userId]/route.ts
+++ src/app/api/cart/[userId]/route.ts
@@ -4,6 +4,7 @@ import { ResponseHandler } from "@/core";
 import DatabaseError from "@/database/db.error";
 import { Cart } from "@/modules";
 import { SCHEMA_NAMES } from "@/utils/constants";
+import { Types } from "mongoose";
 import { NextRequest } from "next/server";
 
 /**
@@ -14,21 +15,25 @@ import { NextRequest } from "next/server";
  */
 export async function GET(req: NextRequest, { params }: { params: { userId: string } }) {
   try {
-    const cart = await Cart.populate({ user: params.userId },
-      {
-        path: "items.artwork",
+    const cart = await Cart.findOne({ user: new Types.ObjectId(params.userId) });
+    if (!cart) return ResponseHandler.error({ message: "Cart not found" }, 404);
+
+    await cart.populate({
+      path: "items",
+      model: SCHEMA_NAMES.CART_ITEM,
+      populate: {
+        path: "artwork",
         model: SCHEMA_NAMES.ARTWORK,
         populate: {
           path: "artist",
           model: SCHEMA_NAMES.ARTIST
         }
       }
-      // TODO: item.id not getting
-      // .projection({ _id: 0, id: "$_id" });
-    );
+    });
 
     return ResponseHandler.success(cart);
   } catch (error) {
+    console.error(error);
     const err = new DatabaseError(error);
     return ResponseHandler.error(err, err.code);
   }

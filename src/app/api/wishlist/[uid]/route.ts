dbd08d2ec46b2482bc0b165373ba47d14cd55d5f
Bhairesh M <bhairesh@arithaconsulting.com>
2024-08-11 22:52:02 +0530

    (api) feature: GET /api/wishlist/[uid] implementation.

--- src/app/api/wishlist/[uid]/route.ts
+++ src/app/api/wishlist/[uid]/route.ts
@@ -1,18 +1,32 @@
 import { ResponseHandler } from "@/core";
 import { User } from "@/modules";
 import type { Params } from "@/types";
+import { SCHEMA_NAMES } from "@/utils/constants";
 import type { NextRequest } from "next/server";
 
-/** /api/wishlist/[uid] */
+/**
+ * Get wishlists
+ * GET /api/wishlist/[uid]
+ * 
+ * Get user's wishlisted artworks and its artist information.
+ */
 const uid = "uid";
 
 export async function GET(req: NextRequest, { params }: Params<typeof uid>) {
   const userId = params.uid;
   if (!userId) return ResponseHandler.error({ userId }, "Missing/Invalid user ID", 400);
 
-  const user = await User.populate({ id: userId }, "wishlist");
+  const user = await User.findById(userId, { password: 0 });
   if (!user) return ResponseHandler.error({ user, userId }, "User not found", 404);
+
+  await user.populate({
+    path: "wishlist",
+    model: SCHEMA_NAMES.ARTWORK,
+    populate: {
+      path: "artist",
+      model: SCHEMA_NAMES.ARTIST
+    }
+  });
 
   return ResponseHandler.success(user);
 }
-

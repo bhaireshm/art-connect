2fdf967f71281d68f06f000d5edd10d90340690e
Bhairesh M <bhairesh@arithaconsulting.com>
2024-08-14 00:51:27 +0530

    (api) update: PUT /api/cart/[userId]/items/[itemId] impl.

--- src/app/api/cart/[userId]/items/[itemId]/route.ts
+++ src/app/api/cart/[userId]/items/[itemId]/route.ts
@@ -5,6 +5,26 @@ import DatabaseError from "@/database/db.error";
 import { Cart } from "@/modules";
 import { NextRequest } from "next/server";
 
+/** 
+ * Update Item in Cart
+ * PUT /api/cart/[userId]/items/[itemId]
+ * 
+ * Updates the quantity of a specific item in the cart. The request body should contain the new quantity.
+ */
+export async function PUT(req: NextRequest, { params }: { params: { userId: string, itemId: string } }) {
+  try {
+    const data = await req.json();
+    const cart = await Cart.update(
+      { user: params.userId, "items.id": params.itemId },
+      { $set: { "items.$.quantity": data.quantity } },
+      { $set: { "items.quantity": data.quantity } },
+    );
+    return ResponseHandler.success(cart);
+  } catch (error) {
+    const err = new DatabaseError(error);
+    return ResponseHandler.error(err, err.code);
+  }
+}
 
 /**
  * Remove Item from Cart
@@ -16,7 +36,7 @@ export async function DELETE(req: NextRequest, { params }: { params: { userId: s
   try {
     const cart = await Cart.update(
       { user: params.userId },
-      { $pull: { items: { _id: params.itemId } } },
+      { $pull: { items: { id: params.itemId } } },
       { new: true }
     );
     return ResponseHandler.success(cart);

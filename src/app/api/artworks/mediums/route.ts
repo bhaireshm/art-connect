a15f948717a3275fdf4306ed56bedbb07da1682e
Bhairesh M <bhairesh@arithaconsulting.com>
2024-08-22 12:33:10 +0530

    fix: /api/artworks/mediums timeout issue.

--- src/app/api/artworks/mediums/route.ts
+++ src/app/api/artworks/mediums/route.ts
@@ -1,8 +1,10 @@
 import { ResponseHandler } from "@/core";
 import { Artwork } from "@/modules";
+import { uniqueArrayOfObjects } from "@bhairesh/ez.js";
 
 /** /api/artworks/mediums */
 export async function GET() {
-  const artworks = await Artwork.m.find({}, { medium: 1 }).distinct("medium");
-  return ResponseHandler.success(artworks);
+  const artworks = await Artwork.findAll({}, { medium: 1 });
+  if (artworks.length === 0) return ResponseHandler.success([], "No artwork mediums found");
+  return ResponseHandler.success(uniqueArrayOfObjects(artworks.map((m: any) => m.medium)));
 }
